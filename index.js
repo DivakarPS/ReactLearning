9b1a6f71b6fdd60a43fc5f991758b5dbe2fd495f
Divakar P S <divaakaar.saakthi@gmail.com>
2023-08-07 07:51:10 +0530

    added react to the project and basic setup

---
+++ index.js
@@ -0,0 +1,25 @@
+import ReactDom from "react-dom";
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"/>
+            </div>
+            <div className="nav-items">
+
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className = "app">
+            //head
+            //body
+            //footer
+        </div>
+    )
+}
