a01fb345d8135b3c44494054c244c023752bd04e
Divakar P S <divaakaar.saakthi@gmail.com>
2023-08-20 18:57:45 +0530

    fixed a bug

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -27,17 +27,29 @@ const RestaurantMenu = () => {
 
     }
 
-    const  {name,cloudinaryImageId,costForTwoMessage,cuisines} = resInfo.cards[0].card.card.info;
+    const  {name,cloudinaryImageId,costForTwoMessage,cuisines} = resInfo?.cards[0]?.card?.card?.info;
     const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
-
+    console.log(itemCards);
     return (
         <div>
             <h1>{name}</h1>
-            <h2>{cuisines.join(", ")} - {costForTwoMessage}</h2>
+            {itemCards? && (
+                <h2>{cuisines.join(", ")} - {costForTwoMessage}</h2>
+            ):null}
             <ul>
-                {itemCards.map(item =>
-                    (<li key={item.card.info.name}>{item.card.info.name} - {item.card.info.price? item.card.info.price/100:item.card.info.defaultPrice/100 }</li>)
-                )}
+                {/*{if(itemCards)?*/}
+                {/*itemCards.map(item =>*/}
+                {/*    (<li key={item.card.info.name}>{item.card.info.name} - {item.card.info.price? item.card.info.price/100:item.card.info.defaultPrice/100 }</li>)*/}
+                {/*):null*/}
+                {/*}*/}
+                {itemCards ?
+        itemCards.map(item => (
+            <li key={item.card.info.name}>
+                {item.card.info.name} - {item.card.info.price ? item.card.info.price / 100 : item.card.info.defaultPrice / 100}
+            </li>
+        ))
+        : <h1>No Menu for now :(</h1>
+    }
             </ul>
         </div>
     )
